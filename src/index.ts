88606e8864b60d57cdb11b3b67ffc3906365d061
yzydeveloper <wqrdw@qq.com>
2022-05-06 00:38:05 +0800

    feat: finish

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
+import type { Plugin } from 'vite'
+
 const VITE_PLUGIN_NAME = 'vite-plugin-resolve-inject'
+
+function PluginResolveInject(): Plugin {
+    return {
+        name: VITE_PLUGIN_NAME,
+        enforce: 'post',
+        resolveId(id) {
+            // 重写路径尝试去node_modules中解析
+            // PR: https://github.com/vitejs/vite/pull/8015
+            return `/node_modules/${id}`
+        }
+    }
+}
+
+export default PluginResolveInject
