a04ac31135805e1288494b56f8c241d68f7dd08e
yzydeveloper <wqrdw@qq.com>
2022-05-08 13:56:10 +0800

    refactor: 依赖vite:resolve重写路径

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import type { Plugin } from 'vite'
+import { join } from 'path'
 
+const RESOLVE_PLUGIN_NAME = 'vite:resolve'
 const VITE_PLUGIN_NAME = 'vite-plugin-resolve-inject'
 
 function PluginResolveInject(): Plugin {
+    let resolvePlugin: Plugin | undefined
+    let root: string
+
     return {
         name: VITE_PLUGIN_NAME,
         enforce: 'post',
-        resolveId(id) {
+        configResolved(config) {
+            resolvePlugin = config.plugins.find(p => p.name === RESOLVE_PLUGIN_NAME)
+            root = config.root
+        },
+        async resolveId(id, ...args) {
             // 重写路径尝试去node_modules中解析
             // PR: https://github.com/vitejs/vite/pull/8015
-            return `/node_modules/${id}`
+            const tryPath = join(root, `/node_modules/${id}`)
+            if (resolvePlugin && resolvePlugin.resolveId) {
+                const res = await resolvePlugin.resolveId.call(this, tryPath, ...args)
+                return res
+            }
+            return id
         }
     }
 }
